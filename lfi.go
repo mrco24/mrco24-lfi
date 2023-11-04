bc24b4a1bfaaad68726a28437fb99ec17d16bc46
mrco24 <95030367+mrco24@users.noreply.github.com>
2023-11-04 20:43:47 +0600

    lfi.go

--- lfi.go
+++ lfi.go
@@ -10,7 +10,6 @@ import (
 	"sync"
 )
 
-// Define ANSI color codes
 const (
 	RED   = "\033[0;31m"
 	CYAN  = "\033[0;36m"
@@ -23,38 +22,24 @@ var (
 )
 
 func main() {
-	// Command-line flags
 	urlsFile := flag.String("u", "urls.txt", "File containing target URLs")
 	payloadsFile := flag.String("p", "payloads.txt", "File containing payloads")
 	outputFile := flag.String("o", "vulnerable_urls.txt", "Output file for vulnerable URLs")
 	verbosity := flag.Bool("v", false, "Enable verbosity for all requests")
 	flag.Parse()
 
-	// Set the verbosity level based on the flag
 	verbose = *verbosity
 
-	// Generate the banner
 	banner := `
-______  ___                      ______ _____ __        ______ _____________ 
-___   |/  /_____________________ __|__ \__  // /        ___  / ___  __/___(_)
-__  /|_/ / __  ___/_  ___/_  __ \____/ /_  // /___________  /  __  /_  __  / 
-_  /  / /  _  /    / /__  / /_/ /_  __/ /__  __/_/_____/_  /____  __/  _  /  
-/_/  /_/   /_/     \___/  \____/ /____/   /_/           /_____//_/     /_/   
-                                                                                                                                       
-                                                
-`
+    ______  ___                      ______ _____ __        ______ _____________
+    ___   |/  /_____________________ __|__ \__  // /        ___  / ___  __/___(_)
+    __  /|_/ / __  ___/_  ___/_  __ \____/ /_  // /___________  /  __  /_  __  /
+    _  /  / /  _  /    / /__  / /_/ /_  __/ /__  __/_/_____/_  /____  __/  _  /
+    /_/  /_/   /_/     \___/  \____/ /____/   /_/           /_____//_/     /_/
+
+	`
 	fmt.Print(CYAN, banner, NC)
 
-	// Check if the URLs and payloads files exist
-	_, urlsErr := os.Stat(*urlsFile)
-	_, payloadsErr := os.Stat(*payloadsFile)
-
-	if urlsErr != nil || payloadsErr != nil {
-		fmt.Println("URLs file or payloads file not found. Make sure both files exist.")
-		return
-	}
-
-	// Read the list of target URLs from the URLs file
 	urlsData, readURLsErr := ioutil.ReadFile(*urlsFile)
 	if readURLsErr != nil {
 		fmt.Println("Error reading URLs file:", readURLsErr)
@@ -62,7 +47,6 @@ _  /  / /  _  /    / /__  / /_/ /_  __/ /__  __/_/_____/_  /____  __/  _  /
 	}
 	urls := strings.Split(string(urlsData), "\n")
 
-	// Read the list of payloads from the payloads file
 	payloadsData, readPayloadsErr := ioutil.ReadFile(*payloadsFile)
 	if readPayloadsErr != nil {
 		fmt.Println("Error reading payloads file:", readPayloadsErr)
@@ -70,7 +54,6 @@ _  /  / /  _  /    / /__  / /_/ /_  __/ /__  __/_/_____/_  /____  __/  _  /
 	}
 	payloads := strings.Split(string(payloadsData), "\n")
 
-	// Initialize the output file
 	output, createOutputErr := os.Create(*outputFile)
 	if createOutputErr != nil {
 		fmt.Println("Error creating output file:", createOutputErr)
@@ -78,10 +61,8 @@ _  /  / /  _  /    / /__  / /_/ /_  __/ /__  __/_/_____/_  /____  __/  _  /
 	}
 	defer output.Close()
 
-	// Create a wait group for synchronization
 	var wg sync.WaitGroup
 
-	// Iterate through URLs and payloads concurrently
 	for _, url := range urls {
 		for _, payload := range payloads {
 			wg.Add(1)
@@ -89,9 +70,7 @@ _  /  / /  _  /    / /__  / /_/ /_  __/ /__  __/_/_____/_  /____  __/  _  /
 				defer wg.Done()
 				fullURL := url + payload
 
-				// Check if the URL is valid before making the request
 				if isValidURL(fullURL) {
-					// Send a GET request to the URL and store the response
 					resp, getErr := http.Get(fullURL)
 					if getErr != nil {
 						if verbose {
@@ -105,7 +84,6 @@ _  /  / /  _  /    / /__  / /_/ /_  __/ /__  __/_/_____/_  /____  __/  _  /
 						fmt.Printf("%sRequest:%s %s\n", GREEN, NC, fullURL)
 					}
 
-					// Check if the response contains "root:" to identify vulnerabilities
 					body, readErr := ioutil.ReadAll(resp.Body)
 					if readErr == nil && strings.Contains(string(body), "root:") {
 						fmt.Printf("%sVulnerable:%s %s\n", RED, NC, fullURL)
@@ -116,11 +94,9 @@ _  /  / /  _  /    / /__  / /_/ /_  __/ /__  __/_/_____/_  /____  __/  _  /
 		}
 	}
 
-	// Wait for all tasks to complete
 	wg.Wait()
 }
 
-// isValidURL checks if a URL is valid
 func isValidURL(url string) bool {
 	_, err := http.Get(url)
 	return err == nil
